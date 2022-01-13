1da02245d7c8840b423008e4acf242115f0db264
Akash Panwar <akashpanwar4411@gmail.com>
2022-01-13 23:01:48 +0530

    small bug fix in createBookingCheckout

--- controller/bookingController.js
+++ controller/bookingController.js
@@ -54,12 +54,12 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) =>{
 //     res.redirect(req.originalUrl.split('?')[0]);
 // });
 
-const createBookingCheckout = async session =>{
+const createBookingCheckout = catchAsync(async session =>{
     const tour = session.client_reference_id;
-    const user = (await User.findOne({email: session.customer_email}) ).id;
-    const price = session.display_items[0].amount / 100;
+    const user = (await User.findOne({email: session.customer_email}) )._id;
+    const price = session.data.amount_total / 100;
     await Booking.create({tour, user, price});
-}
+});
 
 exports.webhookCheckout = (req, res, next) =>{
     const signature = req.headers['stripe-signature'];
